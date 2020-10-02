d536353d39ce6ff18d33ae1dde98a3f2ab2dd326
Restyled.io <commits@restyled.io>
2020-10-02 03:11:49 +0000

    Restyled by clang-format

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
-import commonjs from '@rollup/plugin-commonjs'
-import svelte from 'rollup-plugin-svelte'
-import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
 import glob from 'rollup-plugin-glob'
+import svelte from 'rollup-plugin-svelte'
+import {terser} from 'rollup-plugin-terser'
 import config from 'sapper/config/rollup.js'
-import markdown from './src/utils/markdown.js'
 import sveltePreprocess from 'svelte-preprocess';
+
 import pkg from './package.json'
-import json from '@rollup/plugin-json'
+import markdown from './src/utils/markdown.js'
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
-const onwarn = (warning, onwarn) =>
-  (warning.code === 'CIRCULAR_DEPENDENCY' &&
-    warning.message.includes('/@sapper/')) ||
-  onwarn(warning)
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' &&
+                                     warning.message.includes('/@sapper/')) ||
+                                    onwarn(warning)
 
 const preprocess = sveltePreprocess({
-  preserve: ['ld+json'],
+  preserve : [ 'ld+json' ],
   // ...
 });
 
@@ -29,51 +29,50 @@ export default {
   client: {
     input: config.client.input(),
     output: config.client.output(),
-    plugins: [
-      replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
-      }),
-      svelte({
-        dev,
-        hydratable: true,
-        emitCss: true,
-        preprocess,
-      }),
-      resolve(),
-      commonjs(),
-      markdown(),
-      json(),
-      glob(),
-      legacy &&
-        babel({
-          extensions: ['.js', '.mjs', '.html', '.svelte'],
-          babelHelpers: 'runtime',
-          exclude: ['node_modules/@babel/**'],
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: '> 0.25%, not dead',
-              },
+    plugins:
+        [
+          replace({
+            'process.browser' : true,
+            'process.env.NODE_ENV' : JSON.stringify(mode),
+          }),
+          svelte({
+            dev,
+            hydratable : true,
+            emitCss : true,
+            preprocess,
+          }),
+          resolve(),
+          commonjs(),
+          markdown(),
+          json(),
+          glob(),
+          legacy && babel({
+            extensions : [ '.js', '.mjs', '.html', '.svelte' ],
+            babelHelpers : 'runtime',
+            exclude : [ 'node_modules/@babel/**' ],
+            presets : [
+              [
+                '@babel/preset-env',
+                {
+                  targets : '> 0.25%, not dead',
+                },
+              ],
             ],
-          ],
-          plugins: [
-            '@babel/plugin-syntax-dynamic-import',
-            [
-              '@babel/plugin-transform-runtime',
-              {
-                useESModules: true,
-              },
+            plugins : [
+              '@babel/plugin-syntax-dynamic-import',
+              [
+                '@babel/plugin-transform-runtime',
+                {
+                  useESModules : true,
+                },
+              ],
             ],
-          ],
-        }),
+          }),
 
-      !dev &&
-        terser({
-          module: true,
-        }),
-    ],
+          !dev && terser({
+            module : true,
+          }),
+        ],
     preserveEntrySignatures: false,
     onwarn,
   },
@@ -81,26 +80,26 @@ export default {
   server: {
     input: config.server.input(),
     output: config.server.output(),
-    plugins: [
-      replace({
-        'process.browser': false,
-        'process.env.NODE_ENV': JSON.stringify(mode),
-      }),
-      svelte({
-        generate: 'ssr',
-        dev,
-        preprocess,
-      }),
-      resolve(),
-      commonjs(),
-      json(),
-      markdown(),
-      glob(),
-    ],
-    external: Object.keys(pkg.dependencies).concat(
-      require('module').builtinModules ||
-        Object.keys(process.binding('natives'))
-    ),
+    plugins:
+        [
+          replace({
+            'process.browser' : false,
+            'process.env.NODE_ENV' : JSON.stringify(mode),
+          }),
+          svelte({
+            generate : 'ssr',
+            dev,
+            preprocess,
+          }),
+          resolve(),
+          commonjs(),
+          json(),
+          markdown(),
+          glob(),
+        ],
+    external: Object.keys(pkg.dependencies)
+                .concat(require('module').builtinModules ||
+                        Object.keys(process.binding('natives'))),
 
     onwarn,
   },
@@ -108,15 +107,16 @@ export default {
   serviceworker: {
     input: config.serviceworker.input(),
     output: config.serviceworker.output(),
-    plugins: [
-      resolve(),
-      replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
-      }),
-      commonjs(),
-      !dev && terser(),
-    ],
+    plugins:
+        [
+          resolve(),
+          replace({
+            'process.browser' : true,
+            'process.env.NODE_ENV' : JSON.stringify(mode),
+          }),
+          commonjs(),
+          !dev && terser(),
+        ],
 
     onwarn,
   },
--- src/routes/postApi.js
+++ src/routes/postApi.js
@@ -1,3 +1,1 @@
-export function post(req,res,next){
-    res.send(req.body)
-}
+export function post(req, res, next) { res.send(req.body) }

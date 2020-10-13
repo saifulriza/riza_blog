f12e2095dffa5963bfe0f467051b747b7cfe2114
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 17:46:28 +0700

    [Edit] tes get json

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
   let data;
-  http.get('./blog.json', (res) => {
+  http.get('https://riza-blog-7mays5ci9.vercel.app/blog.json', (res) => {
     data = res | 'gagal';
   })
 // res.setHeader('Content-Type', 'application/xml');

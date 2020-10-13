63d10c40005631022e4f2a7fc3e83c06f7ed989e
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 13:37:15 +0700

    [Add] Toggle dark mode

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,12 @@
 <script>
-  import Logo from './Logo.svelte'
-  import Nav from './Nav.svelte'
+  import Logo from "./Logo.svelte";
+  import Nav from "./Nav.svelte";
 
-  export let segment
+  export let segment;
+
+  function toggle() {
+    window.document.body.classList.toggle("dark-mode");
+  }
 </script>
 
 <style>
@@ -10,12 +14,54 @@
     display: flex;
     margin: 0 auto;
     max-width: 1400px;
-    padding: 2em;
+    padding: 1em;
     width: 100%;
+  }
+  .circle {
+    margin-left: 3em;
+    margin-right: 1em;
+    cursor: pointer;
+    position: relative;
+    border-radius: 100%;
+    width: 2rem;
+    height: 2rem;
+    background: linear-gradient(40deg, #8983f7, #a3dafb 70%);
+
+    box-shadow: -2px 7px 8px 4px #e2e2ea;
+  }
+
+  .circle:hover {
+    box-shadow: 2px 3px 2px gray;
+  }
+
+  :global(body.dark-mode .circle) {
+    background: linear-gradient(40deg, #ff0080, #ff8c00 70%);
+    box-shadow: none;
+  }
+
+  :global(body.dark-mode .circle:hover) {
+    box-shadow: 2px 3px 2px gray;
+  }
+
+  .crescent {
+    position: absolute;
+    border-radius: 100%;
+    right: 0;
+    width: 1.5rem;
+    height: 1.5rem;
+    transform: scale(1);
+    background: #f2eee2;
+    transform-origin: top right;
+    transition: transform 0.6s cubic-bezier(0.445, 0.025, 0.155, 0.5);
+  }
+  :global(body.dark-mode .crescent) {
+    background: white;
+    transform: scale(0);
   }
 </style>
 
 <header>
   <Logo />
   <Nav {segment} />
+  <span class="circle" on:click={toggle}> <span class="crescent" /> </span>
 </header>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Header from '../components/Header.svelte';
+  import Header from "../components/Header.svelte";
 
   export let segment;
 </script>
@@ -25,28 +25,43 @@
     flex-direction: column;
   }
 
+  :global(body.dark-mode main) {
+    flex: 1;
+    position: relative;
+    margin: 0 auto;
+    max-width: 1400px;
+    background-color: #1e212d;
+    padding: 1em 2em;
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  :global(body.dark-mode footer) {
+    color: gray;
+  }
+
   footer {
     color: #000;
     font-size: 1em;
     font-family: Rubik, sans-serif;
-    margin: 1em auto;
+    margin: 0.5em auto;
     max-width: 1400px;
-    padding: 1em 2em;
+    padding: 1em 1em;
     text-align: center;
     width: 100%;
   }
 </style>
 
 <div class="layout">
-  <Header {segment}/>
+  <Header {segment} />
 
   <main>
-    <slot></slot>
+    <slot />
   </main>
 
   <footer>
-    <span>
-      &copy; {new Date().getFullYear()} Saiful Riza's Blog.
-    </span>
+    <span> &copy; {new Date().getFullYear()} Saiful Riza's Blog. </span>
   </footer>
 </div>

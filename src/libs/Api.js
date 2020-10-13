1b0e3aa374500bbf39127a485692f24817532cb7
Restyled.io <commits@restyled.io>
2020-10-13 13:02:54 +0000

    Restyled by prettier

--- src/libs/Api.js
+++ src/libs/Api.js
@@ -1,20 +1,24 @@
 // Api.js
 import axios from "axios";
-require('dotenv').config()
+require("dotenv").config();
 
 // Create a instance of axios to use the same base url.
 const axiosAPI = axios.create({
-  baseURL : process.env.API_URL // it's not recommended to have this info here.
+  baseURL: process.env.API_URL, // it's not recommended to have this info here.
 });
 
 // implement a method to execute all the request from here.
 const apiRequest = (method, url, request) => {
-  const headers = {authorization : ""};
+  const headers = { authorization: "" };
   // using the axios instance to perform the request that received from each
   // http method
-  return axiosAPI({method, url, data : request, headers})
-      .then(res => { return Promise.resolve(res.data); })
-      .catch(err => { return Promise.reject(err); });
+  return axiosAPI({ method, url, data: request, headers })
+    .then((res) => {
+      return Promise.resolve(res.data);
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
 };
 
 // function to execute the http get request
@@ -35,9 +39,9 @@ const patch = (url, request) => apiRequest("patch", url, request);
 // expose your method to other services or actions
 const API = {
   get,
-  delete : deleteRequest,
+  delete: deleteRequest,
   post,
   put,
-  patch
+  patch,
 };
 export default API;

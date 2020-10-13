930c32fc4a96c9458f4be65ddce954159a7978b9
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 14:22:46 +0700

    [Fix] button toggle mode tdk terlihat di mobile

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,10 +3,6 @@
   import Nav from "./Nav.svelte";
 
   export let segment;
-
-  function toggle() {
-    window.document.body.classList.toggle("dark-mode");
-  }
 </script>
 
 <style>
@@ -17,51 +13,9 @@
     padding: 1em;
     width: 100%;
   }
-  .circle {
-    margin-left: 3em;
-    margin-right: 1em;
-    cursor: pointer;
-    position: relative;
-    border-radius: 100%;
-    width: 2rem;
-    height: 2rem;
-    background: linear-gradient(40deg, #8983f7, #a3dafb 70%);
-
-    box-shadow: -2px 7px 8px 4px #e2e2ea;
-  }
-
-  .circle:hover {
-    box-shadow: 2px 3px 2px gray;
-  }
-
-  :global(body.dark-mode .circle) {
-    background: linear-gradient(40deg, #ff0080, #ff8c00 70%);
-    box-shadow: none;
-  }
-
-  :global(body.dark-mode .circle:hover) {
-    box-shadow: 2px 3px 2px gray;
-  }
-
-  .crescent {
-    position: absolute;
-    border-radius: 100%;
-    right: 0;
-    width: 1.5rem;
-    height: 1.5rem;
-    transform: scale(1);
-    background: #f2eee2;
-    transform-origin: top right;
-    transition: transform 0.6s cubic-bezier(0.445, 0.025, 0.155, 0.5);
-  }
-  :global(body.dark-mode .crescent) {
-    background: white;
-    transform: scale(0);
-  }
 </style>
 
 <header>
   <Logo />
   <Nav {segment} />
-  <span class="circle" on:click={toggle}> <span class="crescent" /> </span>
 </header>
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,8 @@
 <script>
   export let segment;
+  function toggle() {
+    window.document.body.classList.toggle("dark-mode");
+  }
 </script>
 
 <style>
@@ -19,7 +22,7 @@
     padding: 10px 5px;
     display: block;
     position: relative;
-    margin-left: 20px;
+    margin-left: 15px;
   }
 
   a:not(.selected) {
@@ -46,6 +49,49 @@
   .selected::before {
     background: #fd6378;
   }
+  .circle {
+    margin-left: 0.5em;
+    margin-right: 0.5em;
+    margin-bottom: auto;
+    margin-top: auto;
+    cursor: pointer;
+    position: relative;
+    border-radius: 100%;
+    width: 2rem;
+    height: 2rem;
+    background: linear-gradient(40deg, #8983f7, #a3dafb 70%);
+
+    box-shadow: -2px 7px 8px 4px #e2e2ea;
+  }
+
+  .circle:hover {
+    box-shadow: 2px 3px 2px gray;
+  }
+
+  :global(body.dark-mode .circle) {
+    background: linear-gradient(40deg, #ff0080, #ff8c00 70%);
+    box-shadow: none;
+  }
+
+  :global(body.dark-mode .circle:hover) {
+    box-shadow: 2px 3px 2px gray;
+  }
+
+  .crescent {
+    position: absolute;
+    border-radius: 100%;
+    right: 0;
+    width: 1.5rem;
+    height: 1.5rem;
+    transform: scale(1);
+    background: #f2eee2;
+    transform-origin: top right;
+    transition: transform 0.6s cubic-bezier(0.445, 0.025, 0.155, 0.5);
+  }
+  :global(body.dark-mode .crescent) {
+    background: white;
+    transform: scale(0);
+  }
 </style>
 
 <nav>
@@ -59,4 +105,5 @@
     rel="prefetch"
     class={segment === 'link' ? 'selected' : ''}
     href="link">link</a>
+  <span class="circle" on:click={toggle}> <span class="crescent" /> </span>
 </nav>

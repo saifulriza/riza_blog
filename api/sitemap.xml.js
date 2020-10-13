770c3510f7f1c000d2bca27f5cea7da34cfc9641
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 18:09:10 +0700

    [Edit] Tes get json

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -1,8 +1,20 @@
 module.exports = (req, res) => {
-  let data;
-  http.get('https://riza-blog-7mays5ci9.vercel.app/blog.json', (res) => {
-    data = res | 'gagal';
-  })
+  var url = 'http://graph.facebook.com/517267866/?fields=picture';
+
+  http.get(url, function(res){
+      var body = '';
+  
+      res.on('data', function(chunk){
+          body += chunk;
+      });
+  
+      res.on('end', function(){
+          var fbResponse = JSON.parse(body);
+          console.log("Got a response: ", fbResponse.picture);
+      });
+  }).on('error', function(e){
+        console.log("Got an error: ", e);
+  });
 // res.setHeader('Content-Type', 'application/xml');
-res.send(data);
+res.send(body);
 }

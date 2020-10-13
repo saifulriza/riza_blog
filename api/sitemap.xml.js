2ec83852071234aaf8cd45eeb3d94b00837dbcdf
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 19:02:06 +0700

    [Add] static route ke sitemap

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -13,11 +13,16 @@ module.exports = (req, res) => {
       resp.on("end", function () {
         var posts = JSON.parse(body);
         const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        <url><loc>https://xn--e28h.my.id/</loc><priority>0.85</priority></url>
+        <url><loc>https://xn--e28h.my.id/about</loc><priority>0.85</priority></url>
+        <url><loc>https://xn--e28h.my.id/blog</loc><priority>0.85</priority></url>
+        <url><loc>https://xn--e28h.my.id/link</loc><priority>0.85</priority></url>
     ${posts
       .map(function (post) {
         return `<url>
                  <loc>${"https://xn--e28h.my.id/blog/" + post.slug}</loc>
+                 <priority>0.69</priority>
             </url>`;
       })
       .join("")}
@@ -26,7 +31,6 @@ module.exports = (req, res) => {
         const xml = render(posts);
         res.setHeader("Content-Type", "application/xml");
         res.send(xml);
-        console.log("Got a response: ", posts.picture);
       });
     })
     .on("error", function (e) {

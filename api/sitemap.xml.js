07f1aaa9cba5fccec9c34d71f1029ade3d5dba64
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 18:12:35 +0700

    [Edit] Tes get json

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -10,11 +10,12 @@ module.exports = (req, res) => {
   
       res.on('end', function(){
           var fbResponse = JSON.parse(body);
+          res.send(fbResponse);
           console.log("Got a response: ", fbResponse.picture);
       });
   }).on('error', function(e){
+    res.send(e)
         console.log("Got an error: ", e);
   });
-// res.setHeader('Content-Type', 'application/xml');
-res.send(body);
+// res.setHeader('Content-Type', 'application/xml')
 }

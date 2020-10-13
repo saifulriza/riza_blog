79d337e98cf685f9dd716ac2b9709062b77dc787
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 18:50:15 +0700

    [Edit] Tes xml response

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -1,23 +1,37 @@
 module.exports = (req, res) => {
-  const https = require('https')
-  var url = 'https://xn--e28h.my.id/blog.json';
+  const https = require("https");
+  var url = "https://xn--e28h.my.id/blog.json";
 
-  https.get(url, function(resp){
-      var body = '';
-  
-      resp.on('data', function(chunk){
-          body += chunk;
+  https
+    .get(url, function (resp) {
+      var body = "";
+
+      resp.on("data", function (chunk) {
+        body += chunk;
       });
-  
-      resp.on('end', function(){
-          var fbResponse = JSON.parse(body);
-          res.setHeader('Content-Type', 'application/xml')
-          res.send(fbResponse);
-          console.log("Got a response: ", fbResponse.picture);
+
+      resp.on("end", function () {
+        var posts = JSON.parse(body);
+        const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${posts
+      .map(function (post) {
+        return `<url>
+                 <loc>${"https://xn--e28h.my.id/blog/" + post.slug}</loc>
+            </url>`;
+      })
+      .join("")}
+    </urlset>
+`;
+        const xml = render(posts);
+        res.setHeader("Content-Type", "application/xml");
+        res.send(xml);
+        console.log("Got a response: ", posts.picture);
       });
-  }).on('error', function(e){
-    res.send(e)
-        console.log("Got an error: ", e);
-  });
-// res.setHeader('Content-Type', 'application/xml')
-}
+    })
+    .on("error", function (e) {
+      res.send(e);
+      console.log("Got an error: ", e);
+    });
+  // res.setHeader('Content-Type', 'application/xml')
+};

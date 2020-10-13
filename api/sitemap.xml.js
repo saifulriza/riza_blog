d99c12a4465866365c904283223359dcad3e64ef
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-13 17:30:45 +0700

    [Add] Header xml

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -1,4 +1,5 @@
 module.exports = (req, res) => {
+res.setHeader('Content-Type', 'application/xml');
 res.send(`<?xml version="1.0" encoding="UTF-8" ?>
   <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     

9cdf62a7036bc7f1727b898b87c5039c2869e9f6
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-15 11:49:55 +0700

    [Add] Sitemap

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -11,45 +11,26 @@ module.exports = (req, res) => {
       });
 
       resp.on("end", function () {
-        // const a = [
-        //   {
-        //     title: "Hello World",
-        //     description: "Hello world",
-        //     slug: "hello-world-copy",
-        //     excerpt: "",
-        //     printDate: "November 27, 2017",
-        //   },
-        //   {
-        //     title: "Hello World",
-        //     description: "Hello world",
-        //     slug: "hello-world",
-        //     excerpt: "",
-        //     printDate: "November 27, 2017",
-        //   },
-        // ];
-        // const xml = a;
-        //         console.log("Body : ", body);
-        //         console.log("Body[0] : ", body[0]);
-        //         var posts = JSON.parse(body[0]);
-        //         console.log("post : ", posts);
-        //         const xml = `<?xml version="1.0" encoding="UTF-8" ?>
-        //         <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-        //         <url><loc>https://xn--e28h.my.id/</loc><priority>0.85</priority></url>
-        //         <url><loc>https://xn--e28h.my.id/about</loc><priority>0.85</priority></url>
-        //         <url><loc>https://xn--e28h.my.id/blog</loc><priority>0.85</priority></url>
-        //         <url><loc>https://xn--e28h.my.id/link</loc><priority>0.85</priority></url>
-        //     ${posts
-        //       .map(function (post) {
-        //         return `<url>
-        //                  <loc>${"https://xn--e28h.my.id/blog/" + post.slug}</loc>
-        //                  <priority>0.69</priority>
-        //             </url>`;
-        //       })
-        //       .join("")}
-        //     </urlset>
-        // `;
-        res.setHeader("Content-Type", "application/json");
-        res.send(body);
+        var posts = JSON.parse(body);
+        const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
+        <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        <url><loc>https://xn--e28h.my.id/</loc><priority>0.85</priority></url>
+        <url><loc>https://xn--e28h.my.id/about</loc><priority>0.85</priority></url>
+        <url><loc>https://xn--e28h.my.id/blog</loc><priority>0.85</priority></url>
+        <url><loc>https://xn--e28h.my.id/link</loc><priority>0.85</priority></url>
+    ${posts
+      .map(function (post) {
+        return `<url>
+                 <loc>${"https://xn--e28h.my.id/blog/" + post.slug}</loc>
+                 <priority>0.69</priority>
+            </url>`;
+      })
+      .join("")}
+    </urlset>
+`;
+        const xml = render(posts);
+        res.setHeader("Content-Type", "application/xml");
+        res.send(xml);
       });
     })
     .on("error", function (e) {

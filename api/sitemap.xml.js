ddbd575f940126b30e79108ce89e19122247e97a
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-15 11:14:43 +0700

    [edit] ambil value dari array

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -11,7 +11,7 @@ module.exports = (req, res) => {
       });
 
       resp.on("end", function () {
-        const xml = body;
+        const xml = body[0];
         //         console.log("Body : ", body);
         //         console.log("Body[0] : ", body[0]);
         //         var posts = JSON.parse(body[0]);

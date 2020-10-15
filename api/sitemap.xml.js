ca7c28983ad314ff51597c2e1f733f9e697d135b
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-15 08:54:21 +0700

    [Edit] sitemap

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
                  <priority>0.69</priority>
             </url>`;
   }
-  const render = `<?xml version="1.0" encoding="UTF-8" ?>
+  const xml = `<?xml version="1.0" encoding="UTF-8" ?>
         <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
         <url><loc>https://xn--e28h.my.id/</loc><priority>0.85</priority></url>
         <url><loc>https://xn--e28h.my.id/about</loc><priority>0.85</priority></url>
@@ -17,7 +17,6 @@ module.exports = (req, res) => {
     ${url}
     </urlset>
 `;
-  const xml = render(posts);
   res.setHeader("Content-Type", "application/xml");
   res.send(xml);
   // res.setHeader('Content-Type', 'application/xml')
